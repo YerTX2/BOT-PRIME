321598de8ef240c71072caf12ea03614b2fd98ba
ZxYerSixZx <oroscoyerson2019@gmail.com>
2024-11-05 23:47:31 -0500

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -26,9 +26,9 @@ const defaultMenu = {
 
 “ Hola mortal *%name* soy  *⚝Hades⚝*, %greeting ”
 
-     ⋆[ *>INFO DE HADES<* ]
+     ⋆[ *>INFO DE Prime<* ]
  ↡▰▱▰▱▰▱▰▱▰▱▰▱▰▱
- 》* 🪐Estado* :  Hades Público 
+ 》* 🪐Estado* :  Bot prime
  》*🗡️ Baileys* : Hades multi
  》*⌛ Despierto * : %muptime
  》*👥Mortales* : %totalreg
@@ -184,8 +184,8 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
     let pp13 = 'https://telegra.ph/file/ba7c4a3eb7bf3d892b0c8.mp4'
     let pp14 = 'https://tinyurl.com/ymlqb6ml'
     let pp15 = 'https://tinyurl.com/ykv7g4zy'
-    let img = await (await fetch(`https://telegra.ph/file/d535430793cd5cb177c58.mp4`)).buffer()
-    await m.react('⭐')
+    let img = await (await fetch(`https://tinyurl.com/2djjdguu`)).buffer()
+    await m.react('✅')
    // await conn.sendMessage(m.chat, { video: { url: [pp, pp2, pp3, pp4, pp5, pp6, pp7, pp8, pp9, pp10, pp11, pp12, pp13, pp14, pp15].getRandom() }, gifPlayback: true, caption: text.trim(), mentions: [m.sender] }, { quoted: estilo })
     await conn.sendFile(m.chat, img, 'thumbnail.jpg', text.trim(), m, null, rcanal)
    //await conn.sendAi(m.chat, botname, textbot, text.trim(), img, img, canal, estilo)
